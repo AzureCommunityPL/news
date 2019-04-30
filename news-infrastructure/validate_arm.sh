1eacf9c5687ae92be5fa26c59bc3c60787fed977
Paweł Ruciński <meanin@wp.pl>
2019-04-30 14:14:05 +0200

    Added validate_arm.sh script

---
+++ news-infrastructure/validate_arm.sh
@@ -0,0 +1,16 @@
+
+isError=0
+for template in $(ls *.json)
+do
+    echo Validating ARM Template: $template
+    error=$(az group deployment validate --resource-group dev --template-file $template --query "error")
+    if [ ! -z "$error" ]; then
+        echo $error
+        isError=1
+    fi
+done
+
+if [ $isError -eq 1 ]; then
+    echo There is an error
+    exit 1
+fi
