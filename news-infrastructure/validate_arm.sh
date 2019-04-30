7a6e23643a62d05a62a959c859a8e312a9508bc5
Paweł Ruciński <meanin@wp.pl>
2019-04-30 18:48:04 +0200

    Skip files from reserved list

--- news-infrastructure/validate_arm.sh
+++ news-infrastructure/validate_arm.sh
@@ -1,6 +1,12 @@
 isError=0
+reserved=( "metadata.json" )
 for template in $(ls *.json)
 do
+    if [[ $(printf "_[%s]_" "${reserved[@]}") =~ .*_\[$template\]_.* ]]; then 
+        echo "$template is present on a reserved filenames list, won't be validated";
+        continue
+    fi
+
     echo Validating ARM Template: $template
     error=$(az group deployment validate --resource-group dev-news-app --template-file $template --query "error")
     if [ ! -z "$error" ]; then
