660d74eb1d2b281c9b65227ea69bc93ad4f06376
Maciej Misztal <mmisztal1980@gmail.com>
2019-05-01 21:45:36 +0200

    tick inversion + tests

--- news-web/src/app/_shared/utils/date.service.spec.ts
+++ news-web/src/app/_shared/utils/date.service.spec.ts
@@ -22,6 +22,18 @@ describe('DateService', () => {
     expect(result).toBe(expectedTicks);
   });
 
+  it('should invert tail-log-ticks for Mon, 29 Apr 2019 00:00:00 GMT', () => {
+    // Arrange
+    const expectedDate = new Date('Mon, 29 Apr 2019 00:00:00 GMT');
+    const ticks = 2518457184000000000;
+
+    // Act
+    const result: Date = service.getDate(ticks);
+
+    // Assert
+    expect(result).toEqual(expectedDate);
+  });
+
   it('should calculate tail-log-ticks for Sun, 28 Apr 2019 00:00:00 GMT', () => {
     // Arrange
     const date = new Date('Sun, 28 Apr 2019 00:00:00 GMT');
@@ -34,6 +46,18 @@ describe('DateService', () => {
     expect(result).toBe(expectedTicks);
   });
 
+  it('should invert tail-log-ticks for Sun, 28 Apr 2019 00:00:00 GMT', () => {
+    // Arrange
+    const expectedDate = new Date('Sun, 28 Apr 2019 00:00:00 GMT');
+    const ticks = 2518458048000000000;
+
+    // Act
+    const result: Date = service.getDate(ticks);
+
+    // Assert
+    expect(result).toEqual(expectedDate);
+  });
+
   it('should calculate tail-log-ticks for Sat, 27 Apr 2019 00:00:00 GMT', () => {
     // Arrange
     const date = new Date('Sat, 27 Apr 2019 00:00:00 GMT');
@@ -44,5 +68,17 @@ describe('DateService', () => {
 
     // Assert
     expect(result).toBe(expectedTicks);
+  });
+
+  it('should invert tail-log-ticks for Sat, 27 Apr 2019 00:00:00 GMT', () => {
+    // Arrange
+    const expectedDate = new Date('Sat, 27 Apr 2019 00:00:00 GMT');
+    const ticks = 2518458912000000000;
+
+    // Act
+    const result: Date = service.getDate(ticks);
+
+    // Assert
+    expect(result).toEqual(expectedDate);
   });
 });
--- news-web/src/app/_shared/utils/date.service.ts
+++ news-web/src/app/_shared/utils/date.service.ts
@@ -1,13 +1,23 @@
 export class DateService {
+    private readonly maxTicks = 3155378112000000000;
+    private readonly epochTicks = 621355968000000000;
+    private readonly ticksPerMs = 10000;
+
     /**
      * Converts a Date (at midnight UTC) to ticks
      * @param date The Date object
      */
     getTicks(date: Date): number {
-        const maxTicks = 3155378112000000000;
-        const epochTicks = 621355968000000000;
-        const ticksPerMs = 10000;
+        return this.maxTicks - ((date.setUTCHours(0, 0, 0, 0) * this.ticksPerMs) + this.epochTicks);
+    }
 
-        return maxTicks - ((date.setUTCHours(0, 0, 0, 0) * ticksPerMs) + epochTicks);
+    /**
+     * Converts tail-log ticks back to Date
+     * @param ticks The tail log ticks
+     */
+    getDate(ticks: number): Date {
+        const utcTicks = this.maxTicks - this.epochTicks - ticks;
+
+        return new Date(utcTicks / this.ticksPerMs);
     }
 }
