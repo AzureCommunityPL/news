0dbbaf8908577c36395d063b3d538f0ce8fbe58d
Maciej Misztal <mmisztal1980@gmail.com>
2019-05-01 21:53:16 +0200

    test refactoring. tests are now data driven

--- news-web/src/app/_shared/utils/date.service.spec.ts
+++ news-web/src/app/_shared/utils/date.service.spec.ts
@@ -10,75 +10,30 @@ describe('DateService', () => {
     expect(service).not.toBe(undefined);
   });
 
-  it('should calculate tail-log-ticks for Mon, 29 Apr 2019 00:00:00 GMT', () => {
-    // Arrange
-    const date = new Date('Mon, 29 Apr 2019 00:00:00 GMT');
-    const expectedTicks = 2518457184000000000;
+  const testData = [
+    {
+      date: new Date('Mon, 29 Apr 2019 00:00:00 GMT'),
+      ticks: 2518457184000000000
+    },
+    {
+      date: new Date('Sun, 28 Apr 2019 00:00:00 GMT'),
+      ticks: 2518458048000000000
+    },
+    {
+      date: new Date('Sat, 27 Apr 2019 00:00:00 GMT'),
+      ticks: 2518458912000000000
+    }
+  ];
 
-    // Act
-    const result: number = service.getTicks(date);
+  testData.forEach(x => {
+    it(`should calculate tail-log-ticks for ${x.date}`, () => {
+      const result: number = service.getTicks(x.date);
+      expect(result).toBe(x.ticks);
+    });
 
-    // Assert
-    expect(result).toBe(expectedTicks);
-  });
-
-  it('should invert tail-log-ticks for Mon, 29 Apr 2019 00:00:00 GMT', () => {
-    // Arrange
-    const expectedDate = new Date('Mon, 29 Apr 2019 00:00:00 GMT');
-    const ticks = 2518457184000000000;
-
-    // Act
-    const result: Date = service.getDate(ticks);
-
-    // Assert
-    expect(result).toEqual(expectedDate);
-  });
-
-  it('should calculate tail-log-ticks for Sun, 28 Apr 2019 00:00:00 GMT', () => {
-    // Arrange
-    const date = new Date('Sun, 28 Apr 2019 00:00:00 GMT');
-    const expectedTicks = 2518458048000000000;
-
-    // Act
-    const result: number = service.getTicks(date);
-
-    // Assert
-    expect(result).toBe(expectedTicks);
-  });
-
-  it('should invert tail-log-ticks for Sun, 28 Apr 2019 00:00:00 GMT', () => {
-    // Arrange
-    const expectedDate = new Date('Sun, 28 Apr 2019 00:00:00 GMT');
-    const ticks = 2518458048000000000;
-
-    // Act
-    const result: Date = service.getDate(ticks);
-
-    // Assert
-    expect(result).toEqual(expectedDate);
-  });
-
-  it('should calculate tail-log-ticks for Sat, 27 Apr 2019 00:00:00 GMT', () => {
-    // Arrange
-    const date = new Date('Sat, 27 Apr 2019 00:00:00 GMT');
-    const expectedTicks = 2518458912000000000;
-
-    // Act
-    const result: number = service.getTicks(date);
-
-    // Assert
-    expect(result).toBe(expectedTicks);
-  });
-
-  it('should invert tail-log-ticks for Sat, 27 Apr 2019 00:00:00 GMT', () => {
-    // Arrange
-    const expectedDate = new Date('Sat, 27 Apr 2019 00:00:00 GMT');
-    const ticks = 2518458912000000000;
-
-    // Act
-    const result: Date = service.getDate(ticks);
-
-    // Assert
-    expect(result).toEqual(expectedDate);
+    it(`should invert tail-log-ticks for ${x.date}`, () => {
+      const result: Date = service.getDate(x.ticks);
+      expect(result).toEqual(x.date);
+    });
   });
 });
