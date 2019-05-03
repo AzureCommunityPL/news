c9be8481c716d948fcf47d7a537b0737a31077aa
Pawel Haracz <haracz5@live.com>
2019-05-03 09:33:14 +0200

    replace guid to string

--- news-functions/Models/CommentDto.cs
+++ news-functions/Models/CommentDto.cs
@@ -5,7 +5,7 @@ namespace NewsFunctions.Models
     public class CommentDto
     {
         public DateTime PostDateTime { get; set; }
-        public Guid PostId { get; set; }
+        public string PostId { get; set; }
         public string Comment { get; set; }
         public string Title { get; set; }
         public string FbUserId { get; set; }
