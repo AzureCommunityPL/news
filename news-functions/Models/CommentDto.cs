de9024cd2f1f0ec71ca55e8a22affc42f6f4e387
Pawel Haracz <haracz5@live.com>
2019-05-04 16:25:57 +0200

    make more rest :)

--- news-functions/Models/CommentDto.cs
+++ news-functions/Models/CommentDto.cs
@@ -4,8 +4,6 @@ namespace NewsFunctions.Models
 {
     public class CommentDto
     {
-        public DateTime PostDateTime { get; set; }
-        public string PostId { get; set; }
         public string Comment { get; set; }
         public string Title { get; set; }
     }
