183dbe1a5381bd692a15ac275c3bac71dcd603cd
Pawel Haracz <haracz5@live.com>
2019-05-04 21:44:12 +0200

    add partition aggregation

--- news-functions/Handlers/GetStorageConnection.cs
+++ news-functions/Handlers/GetStorageConnection.cs
@@ -26,35 +26,68 @@ namespace NewsFunctions.Handlers
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            var policy = new SharedAccessTablePolicy()
+            try
             {
-                SharedAccessExpiryTime = DateTime.UtcNow.AddDays(1),
-                Permissions = SharedAccessTablePermissions.Query
-            };
-            log.LogInformation("created sas policy");
+                var policy = new SharedAccessTablePolicy()
+                {
+                    SharedAccessExpiryTime = DateTime.UtcNow.AddDays(1),
+                    Permissions = SharedAccessTablePermissions.Query
+                };
+                log.LogInformation("created sas policy");
 
-            var storageAccount = CloudStorageAccount.Parse(EnvironmentHelper.GetEnv("AccountStorage-Conn"));
-            var tableClient = storageAccount.CreateCloudTableClient() ??
-                              throw new Exception(nameof(storageAccount.CreateCloudTableClient));
-            log.LogInformation($"Connected to storage account");
+                var storageAccount = CloudStorageAccount.Parse(EnvironmentHelper.GetEnv("AccountStorage-Conn"));
+                var tableClient = storageAccount.CreateCloudTableClient() ??
+                                  throw new Exception(nameof(storageAccount.CreateCloudTableClient));
+                log.LogInformation($"Connected to storage account");
 
-            var table = tableClient.GetTableReference(tableName);
+                var table = tableClient.GetTableReference(tableName);
 
-            log.LogInformation($"Connected to table {tableName}");
+                var query = new TableQuery<NewsTable>().Select(new List<string>() {nameof(NewsTable.PartitionKey)});
 
-            var sasToken = table.GetSharedAccessSignature(policy);
-            log.LogInformation($"Generated sas token {sasToken}");
-            if (string.IsNullOrEmpty(sasToken))
-            {
-                log.LogError("Token is null");
-                throw new NullReferenceException("Token is null");
+                var list = new List<string>();
+                TableContinuationToken continuationToken = null;
+                do
+                {
+                    var resultSegment = await table.ExecuteQuerySegmentedAsync(query, continuationToken);
+                    continuationToken = resultSegment.ContinuationToken;
+
+                    foreach (var entity in resultSegment.Results.Distinct()
+                        .Where(l => list.Contains(l.PartitionKey) == false))
+                    {
+                        list.Add(entity.PartitionKey);
+                    }
+
+                } while (continuationToken != null);
+
+                log.LogInformation($"fetched all records");
+
+                log.LogInformation($"Connected to table {tableName}");
+                var numberGroups = list.GroupBy(p => p);
+
+                var sasToken = table.GetSharedAccessSignature(policy);
+                log.LogInformation($"Generated sas token {sasToken}");
+
+                if (string.IsNullOrEmpty(sasToken))
+                {
+                    log.LogError("Token is null");
+                    throw new NullReferenceException("Token is null");
+                }
+
+                return new OkObjectResult(new
+                {
+                    sas = sasToken,
+                    name = tableName,
+                    partionAggregation = numberGroups.Select(grp => new
+                    {
+                        partionKey = grp.Key,
+                        count = grp.Count()
+                    })
+                });
             }
-
-            return new OkObjectResult(new
+            catch (StorageException ex) when (ex.Message.Contains("Not Found"))
             {
-                sas = sasToken,
-                name = tableName
-            });
+                return new NotFoundObjectResult($"Table {tableName} doesn't exist");
+            }
         }
     }
 }
